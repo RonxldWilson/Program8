75a816f7aaebd4a003732c269f8223579e1b94e3
RonxldWilson <ronaldwilson00000@gmail.com>
2021-11-14 02:37:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/program8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.program8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Broadcast Receiver 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
